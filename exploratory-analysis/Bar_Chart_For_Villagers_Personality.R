89fba60e23f65e81f84b230758d6cd5ec940c31f
Bpppc <91906297+Bpppc@users.noreply.github.com>
2021-11-19 20:48:09 -0800

    Update and rename Villagers_Personality_Bar_Chart3.R to Bar_Chart_For_Villagers_Personality.R

--- exploratory-analysis/Bar_Chart_For_Villagers_Personality.R
+++ exploratory-analysis/Bar_Chart_For_Villagers_Personality.R
@@ -1,4 +1,7 @@
 library(ggplot2)
+library(readxl)
+library(tidyverse)
+library(dplyr)
 
 Villagers <- read.csv("https://raw.githubusercontent.com/info201b-2021-aut/final-project-vickiekknight/main/data/villagers.csv?token=AV5GB6I25KBECUK3IA3OMOTBUGNL2")
 
