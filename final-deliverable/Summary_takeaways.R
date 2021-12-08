86f5aafa7d0f6bb26c3b1cb72c47202c3e9e0142
Bpppc <91906297+Bpppc@users.noreply.github.com>
2021-12-07 19:25:35 -0800

    Update and rename Summary takeaways.R to Summary_takeaways.R

---
+++ final-deliverable/Summary_takeaways.R
@@ -0,0 +1,8 @@
+########### 
+library(shiny)
+library(ggplot2)
+library(RColorBrewer)
+library(plotly)
+
+###########
+read.csv("https://github.com/info201b-2021-aut/final-project-vickiekknight/blob/main/data/fish.csv")
