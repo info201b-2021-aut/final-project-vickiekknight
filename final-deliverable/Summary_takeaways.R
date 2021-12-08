65d9250f68d08f6f3ea2adf4f512699989f4ae6e
Bpppc <91906297+Bpppc@users.noreply.github.com>
2021-12-07 20:05:54 -0800

    Update Summary_takeaways.R

--- final-deliverable/Summary_takeaways.R
+++ final-deliverable/Summary_takeaways.R
@@ -6,3 +6,54 @@ library(plotly)
 
 ###########
 read.csv("https://github.com/info201b-2021-aut/final-project-vickiekknight/blob/main/data/fish.csv")
+
+###########
+introduction <- tabPanel()
+
+interactive_one <- tabPanel()
+
+interactive_two <- tabPanel()
+
+interactive_three <- tabPanel()
+
+Takeaways <- tabPanel(
+  
+  tags$h3(id = "tabs", "What we have learn from our data"),
+  
+  tags$body(
+    
+    tags$header(
+      tags$h1(id = "header", "Three Takeaways"),
+    ),
+    
+    tags$hr(),
+    
+    tags$main(
+      
+      tags$section(
+        tags$h1("Subtitle"),
+        tags$p("Main sentence")
+      ),
+      
+      tags$section(
+        tags$h1("Subtitle"),
+        tags$p("Main sentence")
+      ),
+      
+      tags$section(
+        tags$h1("Subtitle"),
+        tags$p("Main sentnce")
+      )
+    )
+  )
+)
+
+###########
+ui <- navbarPage(
+    title = NULL,
+    introduction,
+    interactive_one,
+    interactive_two,
+    interactive_three,
+    Takeaways
+  )
