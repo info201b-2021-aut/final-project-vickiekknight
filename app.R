911fe67a2406b20689fd8f01109bff5c65d95f1f
Bpppc <91906297+Bpppc@users.noreply.github.com>
2021-12-08 22:07:55 -0800

    Delete app.R

--- app.R
+++
@@ -1,9 +0,0 @@
-# Load the shiny
-library("shiny")
-
-## Source "app_ui.R" and "app_server.R" files.
-source("app_ui.R")
-source("app_server.R")
-
-### Create a new shinyApp
-shinyApp(ui = ui, server = server)
