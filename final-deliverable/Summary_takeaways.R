eebcf684a34e69605cd53287bf52649f77a1b4c5
Bpppc <91906297+Bpppc@users.noreply.github.com>
2021-12-08 19:09:20 -0800

    Update Summary_takeaways.R

--- final-deliverable/Summary_takeaways.R
+++ final-deliverable/Summary_takeaways.R
@@ -31,19 +31,19 @@ Takeaways <- tabPanel(
     tags$main(
       
       tags$section(
-        tags$h1("Insects"),
+        tags$h1("Where is the best location to catch your favorites insects in the animal crossing?"),
         tags$p("According to what we have found out there, we have got the trend of what kind 
                 of insects are drawn to different places. Other players can use this information
                 to learn and find out the best location to catch their favorites insects in the areas. ")
       ),
       
       tags$section(
-        tags$h1("Fishs"),
+        tags$h1("Where is the best location to find the most valuable fish in the animal crossing?"),
         tags$p("Main sentence")
       ),
       
       tags$section(
-        tags$h1("Villagers"),
+        tags$h1("What we have learned from the cabbage trading in this game"),
         tags$p("Main sentnce")
       )
     )
