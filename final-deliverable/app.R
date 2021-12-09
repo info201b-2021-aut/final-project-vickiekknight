4a629eccde10807f04aab440bee1b945e2b55352
Bpppc <91906297+Bpppc@users.noreply.github.com>
2021-12-08 19:51:46 -0800

    Delete app.R

--- final-deliverable/app.R
+++
@@ -1,9 +0,0 @@
-# Load libraries to make them available
-library("shiny")
-
-# Source 'app_ui.R' and 'app_server.R' files.
-source("app_ui.R")
-source("app_server.R")
-
-# Creating shinyApp
-shinyApp(ui = ui, server = server)
