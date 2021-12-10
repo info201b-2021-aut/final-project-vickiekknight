440bcbd2db88754a12974f843ef2f9ce17882b96
vickiekknight <73053418+vickiekknight@users.noreply.github.com>
2021-12-10 13:54:22 -0800

    added turnip graphs

--- final-deliverable/app_ui.R
+++ final-deliverable/app_ui.R
@@ -8,45 +8,6 @@ library(dplyr)
 
 #######Graph
 
-# stock market graph information
-# random pattern line chart
-rday <- c(1, 2, 3, 4, 5, 6)
-rprice <- c(90, 126, 60, 50, 99, 88)
-random <- data.frame(rday, rprice)
-
-random_plot <- ggplot(random, aes(x=rday, y=rprice)) +
-  geom_line() + 
-  ylab("price") +
-  xlab("day")
-
-# large spike pattern line chart
-day <- c(1, 2, 3, 4, 5, 6)
-price <- c(99, 154, 220, 154, 99, 90)
-large_spike <- data.frame(day, price)
-
-large_spike_plot <- ggplot(large_spike, aes(x=day, y=price)) +
-  geom_line()
-
-# decreasing pattern line chart
-dday <- c(1, 2, 3, 4, 5, 6)
-dprice <- c(40, 41.2, 90, 140, 600, 140)
-decreasing <- data.frame(dday, dprice)
-
-decreasing_plot <- ggplot(large_spike, aes(x=dday, y=dprice)) +
-  geom_line() +
-  ylab("price") +
-  xlab("day")
-
-# small spike pattern line chart 
-sday <- c(1, 2, 3, 4, 5, 6)
-sprice <- c(40, 44, 90, 140, 200, 140)
-small_spike <- data.frame(sday, sprice)
-
-small_spike_plot <- ggplot(large_spike, aes(x=sday, y=sprice)) +
-  geom_line() +
-  ylab("price") +
-  xlab("day")
-
 ## Source "app_ui.R" and "app_server.R" files.
 source("app_ui.R")
 source("app_server.R")
@@ -203,6 +164,46 @@ interactive_two <- tabPanel(
 ################
 interactive_three <- tabPanel(
   tags$h3(id = "tabs", "example4"),
+  
+  # stock market graph information
+# random pattern line chart
+rday <- c(1, 2, 3, 4, 5, 6)
+rprice <- c(90, 126, 60, 50, 99, 88)
+random <- data.frame(rday, rprice)
+
+random_plot <- ggplot(random, aes(x=rday, y=rprice)) +
+  geom_line() + 
+  ylab("price") +
+  xlab("day")
+
+# large spike pattern line chart
+day <- c(1, 2, 3, 4, 5, 6)
+price <- c(99, 154, 220, 154, 99, 90)
+large_spike <- data.frame(day, price)
+
+large_spike_plot <- ggplot(large_spike, aes(x=day, y=price)) +
+  geom_line()
+
+# decreasing pattern line chart
+dday <- c(1, 2, 3, 4, 5, 6)
+dprice <- c(40, 41.2, 90, 140, 600, 140)
+decreasing <- data.frame(dday, dprice)
+
+decreasing_plot <- ggplot(large_spike, aes(x=dday, y=dprice)) +
+  geom_line() +
+  ylab("price") +
+  xlab("day")
+
+# small spike pattern line chart 
+sday <- c(1, 2, 3, 4, 5, 6)
+sprice <- c(40, 44, 90, 140, 200, 140)
+small_spike <- data.frame(sday, sprice)
+
+small_spike_plot <- ggplot(large_spike, aes(x=sday, y=sprice)) +
+  geom_line() +
+  ylab("price") +
+  xlab("day")
+  
 ui <- fluidPage(
 
     # Application title
